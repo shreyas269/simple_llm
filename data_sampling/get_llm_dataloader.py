3a27e8de3efce898131c165491737384a89361aa
shreyas269 <shreyas.gp269@gmail.com>
2025-02-10 14:48:31 -0600

    A simple llm dataloader

---
+++ data_sampling/get_llm_dataloader.py
@@ -0,0 +1,14 @@
+from torch.utils.data import DataLoader
+
+class LLMDataloader(DataLoader):
+    def llm_dataloader(self, tokenizer, llm_dataset, batch_size, shuffle=True, drop_last=True, num_workers=0):
+        dataloader = DataLoader(
+        llm_dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        drop_last=drop_last,
+        num_workers=num_workers
+        )
+
+        return dataloader
+    
