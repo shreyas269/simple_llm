dc2c216fcaf034cb36edf77ba893d3fde9d8600e
shreyas269 <shreyas.gp269@gmail.com>
2025-02-10 13:49:48 -0600

    Add bpe tokenizer

---
+++ tokenizer/load_BPE_tokenizer.py
@@ -0,0 +1,21 @@
+import tiktoken
+
+class load_bpe_tokenizer:
+    def __init__(self, encoding="o200k_base", model="gpt-4o"):
+        self.encoding = encoding
+        self.model = model
+
+    def get_encoding(self, encoding):
+        if encoding:
+            enc = tiktoken.get_encoding(encoding)
+        else:
+            enc = tiktoken.get_encoding(self.encoding)
+        return enc
+    
+    def get_encoding_for_model(self, model):
+        if model:
+            enc = tiktoken.get_encoding_for_model(model)
+        else:
+            enc = tiktoken.get_encoding_for_model(self.model)
+        return enc
+    
